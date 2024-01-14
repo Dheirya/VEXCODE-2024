61a8d974bc50f85eb33fb260edcdb2b55ccf3b10
Dheirya Tyagi <69175561+Dheirya@users.noreply.github.com>
2024-01-13 19:23:17 -0500

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -3,28 +3,11 @@
 const int DRIVE_SPEED = 110;
 const int TURN_SPEED  = 90;
 const int SWING_SPEED = 90;
+const int INTAKE_SPEED = 127;
+const int WINCH_SPEED = 35;
 
+// https://ez-robotics.github.io/EZ-Template/tutorials/tuning_constants
 void default_constants() {
-  chassis.set_slew_min_power(80, 80);
-  chassis.set_slew_distance(7, 7);
-  chassis.set_pid_constants(&chassis.headingPID, 11, 0, 20, 0);
-  chassis.set_pid_constants(&chassis.forward_drivePID, 0.45, 0, 5, 0);
-  chassis.set_pid_constants(&chassis.backward_drivePID, 0.45, 0, 5, 0);
-  chassis.set_pid_constants(&chassis.turnPID, 5, 0.003, 35, 15);
-  chassis.set_pid_constants(&chassis.swingPID, 7, 0, 45, 0);
-}
-
-void one_mogo_constants() {
-  chassis.set_slew_min_power(80, 80);
-  chassis.set_slew_distance(7, 7);
-  chassis.set_pid_constants(&chassis.headingPID, 11, 0, 20, 0);
-  chassis.set_pid_constants(&chassis.forward_drivePID, 0.45, 0, 5, 0);
-  chassis.set_pid_constants(&chassis.backward_drivePID, 0.45, 0, 5, 0);
-  chassis.set_pid_constants(&chassis.turnPID, 5, 0.003, 35, 15);
-  chassis.set_pid_constants(&chassis.swingPID, 7, 0, 45, 0);
-}
-
-void two_mogo_constants() {
   chassis.set_slew_min_power(80, 80);
   chassis.set_slew_distance(7, 7);
   chassis.set_pid_constants(&chassis.headingPID, 11, 0, 20, 0);
@@ -40,27 +23,24 @@ void exit_condition_defaults() {
   chassis.set_exit_condition(chassis.drive_exit, 80, 50, 300, 150, 500, 500);
 }
 
-void modified_exit_condition() {
-  chassis.set_exit_condition(chassis.turn_exit, 100, 3, 500, 7, 500, 500);
-  chassis.set_exit_condition(chassis.swing_exit, 100, 3, 500, 7, 500, 500);
-  chassis.set_exit_condition(chassis.drive_exit, 80, 50, 300, 150, 500, 500);
+void opponent_side() {
+  // ...
 }
 
-void main_test_auton() {
-  chassis.set_drive_pid(24, DRIVE_SPEED, true);
+void alliance_side() {
+  // ...
+}
+
+void blank_auton() {
+  // AT 0deg
+  chassis.set_drive_pid(48, DRIVE_SPEED, true);
   chassis.wait_drive();
-  chassis.set_turn_pid(90, TURN_SPEED);
-  chassis.wait_drive();
-  chassis.set_drive_pid(24, DRIVE_SPEED, true);
-  chassis.wait_drive();
+  // AT 180deg
   chassis.set_turn_pid(180, TURN_SPEED);
   chassis.wait_drive();
-  chassis.set_drive_pid(24, DRIVE_SPEED, true);
+  chassis.set_drive_pid(48, DRIVE_SPEED, true);
   chassis.wait_drive();
-  chassis.set_turn_pid(270, TURN_SPEED);
-  chassis.wait_drive();
-  chassis.set_drive_pid(24, DRIVE_SPEED, true);
-  chassis.wait_drive();
-  chassis.set_turn_pid(360, TURN_SPEED);
+  // BACK TO 0deg
+  chassis.set_turn_pid(0, TURN_SPEED);
   chassis.wait_drive();
 }
